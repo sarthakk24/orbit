fe5433ab78f6f0496fdcc9a900bf98dd586c00b2
Sarthak Sachdeva <sarthak.sachdeva.73@gmail.com>
2023-07-13 04:16:35 +0530

    add delete problem functionality

---
+++ src/api/problem/controllers/delete.service.ts
@@ -0,0 +1,51 @@
+import { Request, Response, NextFunction } from 'express'
+import Logger from '../../../loaders/logger'
+import { AxiosResponse } from 'axios'
+import { problemInstance } from '../../../services/axios.service'
+import config from '../../../config'
+import { DBInstance } from '../../../loaders/database'
+
+export const handleDelete = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { id } = req.params
+        Logger.debug(id)
+        const problemId = parseInt(id as string)
+
+        const DB = await DBInstance.getInstance()
+        const problemCollection = await DB.getCollection('problems')
+
+        const problemExist = await problemCollection.findOne({
+            id: problemId,
+        })
+
+        if (!problemExist) {
+            throw {
+                status: 404,
+                message: 'Problem not found',
+            }
+        }
+
+        await problemInstance.delete(
+            `/problems/${id}?access_token=${config.sphere.tokens.problem}`
+        )
+
+        await problemCollection.deleteOne({
+            id: problemId,
+        })
+
+        res.status(201).json({
+            success: true,
+            message: 'Problem Deleted successful',
+        })
+    } catch (err) {
+        Logger.error(err)
+        res.status(err.status || 500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+}
--- src/api/problem/router.ts
+++ src/api/problem/router.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { handleCreate } from './controllers/create.service'
 import { isAdmin, validateJWT } from '../../middleware/jwt.service'
+import { handleDelete } from './controllers/delete.service'
 
 const problemRoutes = Router()
 
 problemRoutes.post('/create', validateJWT, isAdmin, handleCreate)
+problemRoutes.delete('/:id', validateJWT, isAdmin, handleDelete)
 
 export default problemRoutes
